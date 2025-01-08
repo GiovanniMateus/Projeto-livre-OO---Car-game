7a950d6eeef9011c2b389f6c7987dcb4bcecfb97
GiovanniMateus <giovannimateus11@gmail.com>
2025-01-08 09:00:22 -0300

    commit

--- points.py
+++ points.py
@@ -1,5 +1,15 @@
-import timer
-from carro_jogo import Carro
+import pygame
+
 class Point:
-    def __init__(self, pontos):
-        self.pontos = pontos
+    def __init__(self, fonte=30, cor=(255,255,255)):
+        self.score = 0 
+        self.fonte = pygame.font.Font(None, fonte)
+        self.cor = cor
+        
+    def atualização_pontuacao(self):
+        self.score +=1
+        
+    def exibir_score(self, tela):
+        texto = f'Score:{self.score}'
+        superficie_texto = self.fonte.render(texto, True, self.cor)
+        tela.blit(superficie_texto,(10,10))
