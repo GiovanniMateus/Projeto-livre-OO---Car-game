43d486807af407c1399585d1ac00d8140213f9b6
GiovanniMateus <giovannimateus11@gmail.com>
2025-01-09 00:35:17 -0300

    Commit

--- main_jogo.py
+++ main_jogo.py
@@ -5,19 +5,33 @@ from tela_jogo import Tela
 from inicializacao_jogo import Inicializacao
 from jogo_jogo import Jogo
 from carro_jogo import Carro
+from gerenciador_pontuacoes import GerenciadorPontuacoes
+from points import Point
                                 
 # Configuração da tela
-tela = Tela(600, 600, (100, 255, 100))  
+tela = Tela(700, 600, (100, 255, 100))  
 
 # Exibindo a inicialização
 inicializacao = Inicializacao(tela)
 inicializacao.aguardar_acao(tela)  # Aguarda o jogador pressionar ENTER para começar
 
+nome_jogador = inicializacao.entrada_nome()
+
+gerenciador = GerenciadorPontuacoes()
+
+pontuacao = Point()
+
 carro = Carro(r'./Downloads/vrum.png', 300, 500)
+
 
 # Inicia o jogo
 jogo = Jogo(tela)
 jogo.executar(pygame.time.Clock(), 120, carro)
 
+jogo.pontuacao.atualização_pontuacao()
+
+gerenciador.salvar_pontuacao(nome_jogador, jogo.pontuacao.score)
+
 pygame.quit()
 sys.exit()
+
